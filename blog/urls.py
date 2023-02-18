4b4188ae67cb2d10df7b5e62304b4c5c2d6c35eb
Eklu Victor Dziedzorm <55409753+Victorspy-web@users.noreply.github.com>
2023-02-18 23:10:58 +0000

    minor fixes

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import *
+from random import randint
 
 app_name = 'blog'
+
 
 urlpatterns = [
     # post views
     path('', post_list, name="post_list"),
-    path('<int:id>/', post_detail, name="post_detail"),
+    path('post/<int:id>/', post_detail, name="post_detail"),
 ]
